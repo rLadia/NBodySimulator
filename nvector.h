514914c236e1165c8a92bad472af42269adcfcbb
Rodelle Ladia Jr <rLadia@gmail.com>
2013-02-24 18:01:00 -0800

    Renamed, added additional mathematical operators

--- nvector.h
+++ nvector.h
@@ -1,62 +1,73 @@
-#ifndef LADIA_CSS342_VECTOR_H
-#define LADIA_CSS342_VECTOR_H
+#ifndef LADIA_CSS342_NVECTOR_H
+#define LADIA_CSS342_NVECTOR_H
 
 #include <cassert>
 #include <cstddef>
 
 /* 
- * Represents an n-dimensional vector
+ * Represents an n-dimensional Vector
  */
 template<typename T, std::size_t D> //type, dimensions
-class Vector
+class NVector
 {
   typedef std::size_t size_t;
 	T val_[D];
   
   public: 
-  Vector(); //default ctor
-  Vector(const T, const T); //x, y
-  Vector(const T, const T, const T); //x, y, z
+  NVector(); //default ctor
+  NVector(const T, const T); //x, y
+  NVector(const T, const T, const T); //x, y, z
 
   T x() const;
   T y() const;
   T z() const;
 
-  //adds the value of input to current vector 
-  void transform(const Vector<T, D> &);
-  static double magnitude(const Vector<T, D> &);
+  void setX(const T);
+  void setY(const T);
+  void setZ(const T);
+
+  //adds the value of input to current NVector 
+  void transform(const NVector<T, D> &);
+  static T dotProduct(const NVector<T, D> &, const NVector<T, D> &);
 
   T& operator[](size_t);
   const T operator[](const size_t) const; 
 
-  Vector<T, D>& operator+=(const Vector<T, D> &);
-  const Vector<T, D> operator+(const Vector<T, D> &);
+  //overloaded mathematical operators
+  NVector<T, D>& operator*=(const double);
+  NVector<T, D>& operator*=(const int);
 
-  Vector<T, D> & operator-=(const Vector<T, D> &);
-  const Vector<T, D> operator-(const Vector<T, D> &);
+  const NVector<T, D> operator*(const double);
+  const NVector<T, D> operator*(const int);
+  
+  NVector<T, D>& operator+=(const NVector<T, D> &);
+  const NVector<T, D> operator+(const NVector<T, D> &);
+
+  NVector<T, D> & operator-=(const NVector<T, D> &);
+  const NVector<T, D> operator-(const NVector<T, D> &);
 };
 
 template<typename T, size_t D>
-T& Vector<T, D>::operator[](size_t index)
+T& NVector<T, D>::operator[](size_t index)
 {
   return val_[index];
 }
 
 template<typename T, size_t D>
-Vector<T, D>::Vector() 
+NVector<T, D>::NVector() 
 {
-  assert(("Vector size must be > 0", D > 0));
+  assert(("NVector size must be > 0", D > 0));
 }
 
 template<typename T, size_t D>
-Vector<T, D>::Vector(const T x, const T y) 
+NVector<T, D>::NVector(const T x, const T y) 
 {
   val_[0] = x;
   val_[1] = y;
 }
 
 template<typename T, size_t D>
-Vector<T, D>::Vector(const T x, const T y, const T z) 
+NVector<T, D>::NVector(const T x, const T y, const T z) 
 {
   val_[0] = x;
   val_[1] = y;
@@ -64,36 +75,99 @@ Vector<T, D>::Vector(const T x, const T y, const T z)
 }
 
 template<typename T, size_t D>
-void Vector<T, D>::transform(const Vector<T, D>& vector) {
+void NVector<T, D>::transform(const NVector<T, D>& NVector) {
   for(int i = 0; i < D; ++i)
-    val_[i] += vector[i];
+    val_[i] += NVector[i];
 }
 
 template<typename T, size_t D>
-T Vector<T, D>::x() const {
+T NVector<T, D>::dotProduct(const NVector<T, D>& NVector1, const NVector<T, D>& NVector2) {
+  T sum = 0;
+  for(int i = 0; i < D; ++i)
+    sum = NVector1.val_[i] * NVector2.val_[i];
+  return sum;
+}
+
+//setters
+template<typename T, size_t D>
+void NVector<T, D>::setX(const T x) {
+  val_[0] = x;
+}
+
+template<typename T, size_t D>
+void NVector<T, D>::setY(const T y) {
+  assert(("y not defined with NVector size less than 2", D >= 1));
+  val_[1] = y;
+}
+
+template<typename T, size_t D>
+void NVector<T, D>::setZ(const T z) {
+  assert(("z not defined with NVector size less than 3", D >= 2));
+  val_[2] = z;
+}
+
+template<typename T, size_t D>
+T NVector<T, D>::x() const {
   return val_[0];
 }
 
+//getters
+
 template<typename T, size_t D>
-T Vector<T, D>::y() const {
-  assert(("y not defined with vector size less than 2", D >= 1));
+T NVector<T, D>::y() const {
+  assert(("y not defined with NVector size less than 2", D >= 1));
   return val_[1];
 }
 
+
 template<typename T, size_t D>
-T Vector<T, D>::z() const {
-  assert(("z not defined with vector size less than 3", D >= 2));
+T NVector<T, D>::z() const {
+  assert(("z not defined with NVector size less than 3", D >= 2));
   return val_[2];
 }
 
 template<typename T, size_t D>
-const T Vector<T, D>::operator[](const size_t index) const {
+const T NVector<T, D>::operator[](const size_t index) const {
   assert(("Index out of bounds", index > 0 && index < D));
   return val_[index];
 }
 
+
+//overloaded operators
 template<typename T, size_t D>
-Vector<T, D>& Vector<T, D>::operator+=(const Vector<T, D> &rhs) 
+NVector<T, D>& NVector<T, D>::operator*=(const double scalar) 
+{
+  for(int i = 0; i < D; ++i)
+    val_[i] *= scalar;
+  return *this;
+}
+
+template<typename T, size_t D>
+const NVector<T, D> NVector<T, D>::operator*(const double scalar) 
+{
+  NVector<T, D> result = *this;
+  result *= scalar;
+  return result;
+}
+
+template<typename T, size_t D>
+NVector<T, D>& NVector<T, D>::operator*=(const int scalar) 
+{
+  for(int i = 0; i < D; ++i)
+    val_[i] *= scalar;
+  return *this;
+}
+
+template<typename T, size_t D>
+const NVector<T, D> NVector<T, D>::operator*(const int scalar) 
+{
+  NVector<T, D> result = *this;
+  result *= rhs;
+  return result;
+}
+
+template<typename T, size_t D>
+NVector<T, D>& NVector<T, D>::operator+=(const NVector<T, D> &rhs) 
 {
   for(int i = 0; i < D; ++i)
     val_[i] += rhs.val_[i];
@@ -101,15 +175,15 @@ Vector<T, D>& Vector<T, D>::operator+=(const Vector<T, D> &rhs)
 }
 
 template<typename T, size_t D>
-const Vector<T, D> Vector<T, D>::operator+(const Vector<T, D> &rhs)
+const NVector<T, D> NVector<T, D>::operator+(const NVector<T, D> &rhs)
 {
-  Vector<T, D> result = *this;
+  NVector<T, D> result = *this;
   result += rhs;
   return result;
 }
 
 template<typename T, size_t D>
-Vector<T, D>& Vector<T, D>::operator-=(const Vector<T, D> &rhs)
+NVector<T, D>& NVector<T, D>::operator-=(const NVector<T, D> &rhs)
 {
   for(int i = 0; i < D; ++i)
     val_[i] -= rhs.val_[i];
@@ -117,13 +191,11 @@ Vector<T, D>& Vector<T, D>::operator-=(const Vector<T, D> &rhs)
 }
 
 template<typename T, size_t D>
-const Vector<T, D> Vector<T, D>::operator-(const Vector<T, D> &rhs) 
+const NVector<T, D> NVector<T, D>::operator-(const NVector<T, D> &rhs) 
 {
-  Vector<T, D> result = *this;
+  NVector<T, D> result = *this;
   result -= rhs;
   return result;
 }
-
-
 
 #endif
