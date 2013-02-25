25b1dad23d482c596a75cb6a36a2b299c45a3471
Rodelle Ladia Jr <rLadia@gmail.com>
2013-02-24 18:01:42 -0800

    Namespace to handle gravity functions

---
+++ gravity.h
@@ -0,0 +1,19 @@
+#ifndef LADIA_CSS342_GRAVITY_H
+#define LADIA_CSS342_GRAVITY_H
+
+#include <cmath>
+#include "nvector.h"
+
+namespace Gravity {
+  static const double G = 6.67398e-11;
+
+  struct PointMass {
+    int mass;
+    NVector<int, 3> location;
+  };
+
+  NVector<double, 3> force(PointMass, PointMass);
+  NVector<double, 3> force(PointMass, PointMass, const double);
+}
+
+#endif
