3fb43762fe879af47b61839a2a413a533174fd11
Rodelle Ladia Jr <rLadia@gmail.com>
2013-02-26 13:45:23 -0800

    fixed error in gravity calculation

--- gravity.cpp
+++ gravity.cpp
@@ -10,7 +10,7 @@ NVector<double, 3> Gravity::force(PointMass m1, PointMass m2, const double g_con
 {
   NVector<double, 3> direction = m2.location - m1.location;
 
-  double dotProduct = NVector<double, 3>::dotProduct(m1.location, m2.location);
-  return direction * (g_constant * m1.mass * m2.mass / pow(dotProduct, 3));
+  double dotProduct = NVector<double, 3>::dotProduct(direction, direction);
+  return direction * (g_constant * m1.mass * m2.mass / pow(dotProduct, 1.5));
 }
 
