7941851f78b6f6fe93d6908e7ef4773995bdd7b5
Rodelle Ladia Jr <rLadia@gmail.com>
2013-03-03 20:14:14 -0800

    Comments; removed unused functions

--- nbodysimulation.h
+++ nbodysimulation.h
@@ -27,7 +27,7 @@ public:
     kBoundary, //collided with the boundary
   };
 
-  //Contains user accessible information about a collision
+  // Contains user accessible information about a collision
   struct Record {
     int index; //the index of the sphere that collided
     Color::Color color;
@@ -35,101 +35,79 @@ public:
     CollisionType collision; // the type of collision that occured
   };
 
-  //adds a sphere to the internal list of bodies to be simulated
+  // adds a sphere to the internal list of bodies to be simulated
   void addBody(Color::Color, const Vector3 &, int, const Vector3 &);
   
-  //adds a black hole to the bodies to be simulated
+  // adds a black hole to the bodies to be simulated
   void addBlackHole(const Vector3 &, int);
-
-  //will remove bodies as they collide
-  //stop when bodies are stable?
-  //void simulate(); //advance the simulation until no more bodies exist
 
   //return: vector containing the information about all of the collisions
   //  that have occurred so far
   std::vector<Record> getSimulationResults();
 
-  // runs the simulation until all spheres are dead
+  // runs the simulation until no bodies exist
+  // *TODO* add check for stable orbits
   void runSimulation(); 
 
   NBodySimulation(unsigned int); //boundary size
 
 private:
+
   //associates a body with an index value and color
   struct ManagedBody {
     int index;
     SimulatedBody body;
     Color::Color color;
   };
-  
-  //contains information about the time and participants in a collision
-  //Natural sorting will order the elements by the time of collision
-  struct Collision {
-    double time;
-    const ManagedBody* left;
-    const ManagedBody* right;
-
-    bool operator< (const Collision &rhs) {
-      return time < rhs.time;
-    }
-  };
 
   unsigned int boundary_; //the length of the bounding cube
   unsigned int index_; //current sphere index
   double time_; //current time of the simulation
 
-  //contains the list of eliminations caused by collisions
+  // contains the list of eliminations caused by collisions
   std::vector<Record> records_;
-  
-  //contains all possible collisions made by the spheres
-  std::list<Collision> collisions_; 
 
   //contains the list of bodies to be simulated
   std::list<ManagedBody> bodies_;
   std::list<SimulatedBody> black_holes_;
-  std::list<ManagedBody> massless_bodies_;
+  std::list<ManagedBody> freemoving_bodies_;
 
   typedef std::list<NBodySimulation::ManagedBody>::iterator ManagedBodyIterator;
   typedef std::list<SimulatedBody>::iterator SimulatedBodyIterator;
-  typedef std::list<MovingSphere>::iterator MasslessBodyIterator;
 
   typedef Color::Color Color;
+
+  void findAllOverlaps();
   
+  // Updates the instantaneous force acting upon all of the simulated bodies
   void updateAllForces();
-  void calculateForcesFromMasslessBodies();
-  void calculateForcesFromBodies();
-  void calculateForcesFromBlackHoles();
 
-  
-  //updates the force of each body 
-  void updateForce(SimulatedBody &, SimulatedBody &);
-
-  // sets all of the forces in the bodies to 0
+  // sets the net force of the simulated bodies to 0
   void resetForces();
 
+  // Calculates the instantaneous forces exerted on the simulated bodies
+  // by each other body, by the free moving bodies, and by the black holes
+  void calculateForcesFromBodies();
+  void calculateForcesFromFreeMovingBodies();
+  void calculateForcesFromBlackHoles();
+
+  // Adds the force exerted on each body to each body's net force 
+  void updateForce(SimulatedBody &, SimulatedBody &);
+  
+  // returns: true if the sphere is overlapping the boundary
   bool isOverlappingBoundary(const Sphere &);
+  
   void removeSmallerBody(ManagedBodyIterator, ManagedBodyIterator);
 
-  //returns the sphere with the smaller radius
-  //if the radii are the same, it returns the first argument
-  static const ManagedBody* smallerSphere(const ManagedBody*, const ManagedBody*);
+  // advances the simulation by the time period given
+  void advance(double); 
 
-  //Iterates through the list and returns the smallest, positive value
-  static double smallestTime(const std::vector<double>&);
-
-  void advance(double); // advances the simulation by the time period given
-
-  //body, time, type
+  // Adds the collision event to the list of recorded events
   void recordEvent(const ManagedBody&, double, CollisionType);
 
-  //Compares each sphere with every other sphere and the boundary. All 
-  //collisions detected by these comparisions will be added to collisions_.
-  //
-  //post: collisions_ appended with all possible collisions made between 
-  //  spheres and the boundary
-  void findAllOverlaps();
+  
 
-  //do not make a copy of this class
+  // do not make a copy of this class
   NBodySimulation(const NBodySimulation &);
   NBodySimulation& operator=(const NBodySimulation &);
 };
