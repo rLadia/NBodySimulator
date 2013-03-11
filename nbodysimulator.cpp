9ca763615418c00cdbf0465dd13ef8ea7d3f0d24
Rodelle Ladia Jr. <rLadia@gmail.com>
2013-03-11 06:59:15 -0700

    Removed collision detection

--- nbodysimulator.cpp
+++ nbodysimulator.cpp
@@ -1,36 +1,16 @@
 #include "nbodysimulator.h"
 #include <boost/foreach.hpp>
 
-typedef NBodySimulator::Record Record;
 typedef boost::shared_ptr<SimulatedBody> BodyPtr;
 
 NBodySimulator::NBodySimulator(int boundary) 
-  : index_(1), boundary_(boundary), time_(0)
+  : boundary_(boundary)
 {}
 
 // adds a body to be simulated
 void NBodySimulator::SetBodyList(const std::list<BodyPtr> &bodies)
 {
-  // initializes with acceleration of 0 and mass equal to radius
-  BOOST_FOREACH(BodyPtr b, bodies) {
-    SimulatedBody body = SimulatedBody(
-      b->getCenter(), 
-      b->getRadius(), 
-      b->getVelocity(), 
-      Vector3(0,0,0),   // initial force set to 0
-      b->getRadius()
-    );
-
-    ManagedBody indexed_body = { index_, body, Color::kBlue, false };
-    bodies_.push_back(indexed_body);
-    index_++;
-  }
-}
-
-//Returns a vector of records detailing how and when the bodies collided
-std::vector<NBodySimulator::Record> NBodySimulator::getSimulationResults()
-{
-  return records_;
+  bodies_ = bodies;
 }
 
 //*TODO* add check for stable orbits
@@ -38,69 +18,44 @@ std::vector<NBodySimulator::Record> NBodySimulator::getSimulationResults()
 void NBodySimulator::RunSimulation(const double timeinterval)
 {
   updateAllForces();
-  advance(timeinterval);
-  recordAndMarkCollisions();
-}
-// removes all simulated bodies and
-// resets simulation to initial state
-void NBodySimulator::reset()
-{
-  bodies_.clear();
-  black_holes_.clear();
-  time_ = 0;
-  index_ = 1;
-}
 
-// calculates the forces for the current frame and advances the spheres by the 
-// given time interval
-void NBodySimulator::advance(const double time) {
-  BOOST_FOREACH(ManagedBody &b, bodies_) {
-    b.body.advance(time);
+  typedef boost::shared_ptr<SimulatedBody> Body;
+  BOOST_FOREACH(Body b, bodies_) {
+    b->advance(timeinterval);
   }
-  time_ += time;
 }
 
 // Updates the instantaneous force exerted on all of the simulated bodies
 void NBodySimulator::updateAllForces()
 {
   resetForces(); //start from 0
-  calculateForcesFromBodies();
+  calculateForcesBetweenBodies();
 }
 
 // set the forces of each simulated body to 0
 void NBodySimulator::resetForces() 
 {
-  typedef std::list<NBodySimulator::ManagedBody>::iterator ManagedBodyIterator;
-  for(ManagedBodyIterator i = bodies_.begin(); i != bodies_.end(); ++i)
-    i->body.setForce(Vector3(0,0,0));
+  for(BodyList::iterator i = bodies_.begin(); i != bodies_.end(); ++i)
+    (*i)->setForce(Vector3(0,0,0));
 }
 
 
 // Calculates the instantaneous forces exerted on the simulated bodies
 // by each other simulated body
-void NBodySimulator::calculateForcesFromBodies() 
+void NBodySimulator::calculateForcesBetweenBodies() 
 {
-  ManagedBodyIterator i = bodies_.begin();
-  while(i != bodies_.end()) {
-    ManagedBodyIterator j = i; //not necessary to start at beginning
+  BodyList::iterator i = bodies_.begin();
 
-    //bodies do apply a force on themselves
+  for(i; i != bodies_.end(); ++i) {
+    BodyList::iterator j = i; //not necessary to start at beginning
+
+    // bodies do not apply a force on themselves
     for(j++; j != bodies_.end(); ++j) {
-      addForcesBetween(i->body, j->body);
+      addForcesBetween(**i, **j);
     }
-    i++;
   };
 }
 
-// Calculates the instantaneous forces exerted on the simulated bodies
-// by the black holes
-void NBodySimulator::calculateForcesFromBlackHoles()
-{
-  for(ManagedBodyIterator i = bodies_.begin(); i != bodies_.end(); ++i) {
-    for(SimulatedBodyIterator j = black_holes_.begin(); j != black_holes_.end(); ++j)
-      addForcesBetween(i->body, *j);
-  }
-}
 
 // Adds the force exerted on each body to each body's net force 
 void NBodySimulator::addForcesBetween(SimulatedBody &b1, SimulatedBody &b2)
@@ -111,138 +66,4 @@ void NBodySimulator::addForcesBetween(SimulatedBody &b1, SimulatedBody &b2)
   Vector3 force = Gravity::force(m1, m2, kGravity);
   b1.setForce(b1.getForce() + force);
   b2.setForce(b2.getForce() + force * -1);
-}
-
-// handles collisions for body-body, body-blackhole and body-boundary
-void NBodySimulator::recordAndMarkCollisions()
-{
-  if(bodies_.size() <= 0) //no spheres to check collisions with
-    return;
-
-  // check if bodies overlap with black holes
-  handleBlackHoleOverlap(bodies_, black_holes_);
-
-  // check if bodies overlap with each other
-  handleBodyOverlap();
-
-  // check if bodies overlap with the boundary
-  handleBoundaryOverlap(bodies_);
-}
-
-void NBodySimulator::handleBodyOverlap()
-{
-  ManagedBodyIterator i;
-
-  // iterate through list of bodies_
-  for(i = bodies_.begin(); i != bodies_.end(); ++i) {
-    ManagedBodyIterator j = i; // Iterator through rest of bodies_
-
-    for(++j; j != bodies_.end(); ++j) { // not necessary to compare to itself
-      if(i->isDead || j->isDead) { // do not check dead spheres
-        // do nothing
-      } else if(COLLISION::isOverlapping(i->body, j->body)) { //overlap found
-
-        // remove the smaller sphere
-        const ManagedBodyIterator* removeThis = toBeRemoved(&i, &j);
-        if(*removeThis == i) {
-          recordAndMarkForDeletion(*i, kCollision);
-          break; // do not compare i with rest of the bodies
-        } else if (*removeThis == j) {
-          recordAndMarkForDeletion(*j, kCollision);
-        }
-      }
-    }
-  } // so much nesting!
-}
-
-const NBodySimulator::ManagedBodyIterator* NBodySimulator::toBeRemoved(
-  const ManagedBodyIterator* left, const ManagedBodyIterator* right)
-{
-  // black spheres do not interact with each other
-  if((*left)->color == Color::kBlack && (*right)->color == Color::kBlack)
-    return NULL; 
-
-  // black spheres win all other interactions
-  if((*left)->color == Color::kBlack)
-    return right;
-  if((*right)->color == Color::kBlack)
-    return left;
-
-  // the smaller sphere will be removed
-  return 
-    (*left)->body.getRadius() <= (*right)->body.getRadius() ? left : right;
-}
-
-void NBodySimulator::removeDeadBodies()
-{
-  ManagedBodyIterator i = bodies_.begin();
-
-  // iterate through list of bodies
-  while(i != bodies_.end()) {
-    if(i->isDead)
-      i = bodies_.erase(i);
-    else // do not double increment
-      ++i;
-  };
-}
-
-void NBodySimulator::recordAndMarkForDeletion(
-  ManagedBody &body, NBodySimulator::CollisionType collsionType)
-{
-  recordEvent(body, time_, collsionType);
-  body.isDead = true;
-}
-
-// if any of the bodies collided with a black hole
-// the event is recorded and the body is removed from the list
-void NBodySimulator::handleBlackHoleOverlap(
-  std::list<ManagedBody> &bodies, const std::list<SimulatedBody> & blackholes)
-{
-  ManagedBodyIterator i; // iterator through list of spheres
-
-  // iterator through list of black holes
-  std::list<SimulatedBody>::const_iterator j; 
-
-  for(i = bodies_.begin(); i != bodies_.end(); ++i) {
-    for(j = blackholes.begin(); j != blackholes.end(); ++j) { 
-      if(COLLISION::isOverlapping(i->body, j->getCenter())) { // overlap found
-        recordAndMarkForDeletion(*i, kBlackHole);
-      }
-    }
-  }
-}
-
-// if any of the bodies collided with the boundary
-// the event is recorded and the body is removed from the simulation
-void NBodySimulator::handleBoundaryOverlap(std::list<ManagedBody> &bodies)
-{
-  ManagedBodyIterator i;
-
-  // iterate through the list of bodies
-  for(i = bodies_.begin(); i != bodies_.end(); ++i) {
-    if(isOverlappingBoundary(i->body)) { // if overlapping
-      recordAndMarkForDeletion(*i, kBoundary);
-    }
-  };
-}
-
-// returns: true if the sphere is overlapping the boundary
-bool NBodySimulator::isOverlappingBoundary(const Sphere &sphere)
-{
-  Vector3 center = sphere.getCenter();
-  int radius = sphere.getRadius();
-
-  for(int i = 0; i < 3; ++i) {
-    if(center[i] + radius >= boundary_ || center[i] - radius <= 0)
-      return true;
-  }
-  return false;
-}
-
- // Adds the collision event to the list of recorded events
-void NBodySimulator::recordEvent(
-  const ManagedBody &body, double time, NBodySimulator::CollisionType type)
-{
-  Record record = { body.index, body.color, time, type };
-  records_.push_back(record);
 }
--- nbodysimulator.h
+++ nbodysimulator.h
@@ -19,65 +19,24 @@ static const int kGravity = 10;
 // Simulates the motion of 3d bodies through space
 class NBodySimulator {
 public:
-  enum CollisionType {
-    kCollision, // collided with another body
-    kBlackHole, // collided with the black hole
-    kBoundary   // collided with the boundary
-  };
+  typedef boost::shared_ptr<SimulatedBody> Body;
+  typedef std::list<Body> BodyList;
 
-  // Contains user accessible information about a collision
-  struct Record {
-    int index; // the index of the sphere that collided
-    Color::Color color;
-    double time; // the time of collision
-    CollisionType collision; // the type of collision that occurred
-  };
-
-  typedef boost::shared_ptr<SimulatedBody> BodyPtr;
-
-  void SetBodyList(const std::list<BodyPtr> &);
-
-  // Returns a vector containing records on all of the events that have 
-  // occurred. Each record consists of the index and color of body that was 
-  // removed, the time of collision, and the type of collision. 
-  std::vector<Record> getSimulationResults();
+  void SetBodyList(const BodyList &);
 
   // Runs the simulation for the time period given
   // sphere. Every time a body collides, a record will be added that can be
   // accessed by calling getSimulationResults()
   void RunSimulation(const double); 
 
-  // removes all simulated bodies and black holes and resets the 
-  // simulation to its initial state
-  void reset();
-
   NBodySimulator(int); //boundary size
 
 private:
 
-  //associates a body with an index value and color
-  struct ManagedBody {
-    int index;
-    SimulatedBody body;
-    Color::Color color;
-    bool isDead;
-  };
-
   unsigned int boundary_; //the length of the bounding cube
-  unsigned int index_; //current sphere index
-  double time_; //current time of the simulation
-
-  // contains the list of eliminations caused by collisions
-  std::vector<Record> records_;
 
   //contains the list of bodies to be simulated
-  std::list<ManagedBody> bodies_;
-  std::list<SimulatedBody> black_holes_;
-
-  typedef std::list<NBodySimulator::ManagedBody>::iterator ManagedBodyIterator;
-  typedef std::list<SimulatedBody>::iterator SimulatedBodyIterator;
-
-  typedef Color::Color Color;
+  BodyList bodies_;
   
   // Sets the net force of each simulated body to 0
   void resetForces();
@@ -89,55 +48,14 @@ private:
 
   // Calculates the instantaneous forces exerted on the simulated bodies
   // from each other body and from the black hole
-  void calculateForcesFromBodies();
-  void calculateForcesFromBlackHoles();
+  void calculateForcesBetweenBodies();
 
   // Adds the gravitational force between each body to each body's net force
   void addForcesBetween(SimulatedBody &, SimulatedBody &);
   
-  // Compares the simulated bodies with the black holes, each other and the 
-  // boundary. If any overlaps are found, the event is recorded as a collision
-  // and the body is marked as ready to be removed from the simulation
-  void recordAndMarkCollisions();
-
-  // Compares each body to each other body and looks for overlaps.
-  // If any overlaps are found, the smaller body is marked for removal and the
-  // event is recorded as a collision. Black spheres are bigger than all other 
-  // spheres for purposes of comparisons.
-  void handleBodyOverlap();
-
-  // Compares each body to each black hole and looks for overlaps. If any 
-  // overlaps are found, the body is marked for removal and the event 
-  // is recorded as a black hole collision.
-  void handleBlackHoleOverlap(
-    std::list<ManagedBody> &, const std::list<SimulatedBody> &);
-
-  // Compares each body to the boundary and looks for overlaps. If any 
-  // overlaps are found, the body is marked for removal and the event 
-  // is recorded as a boundary collision.
-  void handleBoundaryOverlap(std::list<ManagedBody> &);
-
-  // Returns true if the sphere is in contact over overlapping the boundary
-  bool isOverlappingBoundary(const Sphere &);
-
-  // Removes bodies from the simulation that have lost collisions
-  void removeDeadBodies();
-
-  // Returns the body that should be deleted in a collision.
-  // If neither body should be deleted, this function returns NULL.
-  // *TODO* replace with decision matrix?
-  const ManagedBodyIterator* toBeRemoved(
-    const ManagedBodyIterator*, const ManagedBodyIterator*);
-
-  // Records the event and marks the body as ready to be removed from the list
-  void recordAndMarkForDeletion(ManagedBody &, CollisionType);
-  
   // Advances the simulation by the time period given
   // Each body will move according to its current velocity and the net force
   void advance(const double); 
-
-  // Adds the collision event to the list of recorded events
-  void recordEvent(const ManagedBody&, double, CollisionType);
 };
 
 #endif
