b01fd85f56b1ebce0fe6ea465a358cbd30a88357
Rodelle Ladia Jr. <rLadia@gmail.com>
2013-03-23 16:26:30 -0700

    Implemented constructors and Advance

--- ModelObject.cpp
+++ ModelObject.cpp
@@ -1,4 +1,34 @@
 #include "ModelObject.h"
+#include <cmath>
+
+// default ctor
+ModelObject::ModelObject()
+  : position_(0, 0, 0), velocity_(0, 0, 0), force_(0, 0, 0), mass_(0), radius_(0)
+{}
+
+// position, velocity, force, mass, radius
+ModelObject::ModelObject(
+  Vector3D position, 
+  Vector3D velocity, 
+  Vector3D force, 
+  double mass, 
+  double radius)
+  : position_(position), 
+    velocity_(velocity), 
+    force_(force), 
+    mass_(mass), 
+    radius_(radius)
+{}
+
+void ModelObject::Advance(double time)
+{
+  Vector3 acceleration = force_ * (1.0f / mass_);
+
+  position_ += velocity_ * time; // v(0) * t
+  position_ += acceleration * 0.5 * std::pow(time, 2.0); // 1/2 * a * t^2
+
+  velocity_ += acceleration * time; // v(t) = v(0) + at
+}
 
 // getters
 const ModelObject::Vector3D ModelObject::Position() const
