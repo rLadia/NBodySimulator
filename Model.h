b81a77293042321ee3c21bc176901428967d779a
Rodelle Ladia Jr. <rLadia@gmail.com>
2013-03-23 17:03:14 -0700

    Added declaration for Update

--- Model.h
+++ Model.h
@@ -6,10 +6,12 @@ elapsed. (later on, any additional input that comes from the user). The model
 calculates the effect that the initial conditions have on the model and 
 returns the data in a parseable fashion.
 
-
 References the rules for determining how the objects in the model interact 
 with each other.
 */
+
+#include <vector>
+#include "ModelObject.h"
 
 #ifndef LADIA_NBODYSIMULATOR_MODEL_H
 #define LADIA_NBODYSIMULATOR_MODEL_H
@@ -17,9 +19,16 @@ with each other.
 class Model
 {
 public:
-  Model();
-private:
+  typedef std::vector<ModelObject> ObjectList;
 
+  Model();
+
+  // updates state of model after the given time interval has passed
+  // The objects can have their positions modified or be removed from the list
+  void Update(ObjectList &, double);
+
+private:
+  ObjectList objects_;
 };
 
 #endif
