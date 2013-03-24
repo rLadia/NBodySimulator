d5f42b4db0d62aca32577247e118902be95d9291
Rodelle Ladia Jr. <rLadia@gmail.com>
2013-03-23 19:45:09 -0700

    Modified to use shapes

--- collision.cpp
+++ collision.cpp
@@ -1,42 +1,20 @@
 #include "collision.h"
-#include "polynomial.h"
+#include <cmath>
 
 //if the sum of the radius is <= the distance, the spheres collide
-bool COLLISION::isOverlapping(const Sphere& left, const Sphere& right){
-  return distance(left.getCenter(), right.getCenter()) 
-    <= left.getRadius() + right.getRadius();
+bool Overlap::isOverlapping(Shape::Circle left, Shape::Circle right){
+  return distance(left.center, right.center) 
+    <= left.radius + right.radius;
 }
 
 //Returns whether the point exists within the sphere
-bool COLLISION::isOverlapping(const Sphere& left, const COLLISION::Point& right){
-  return distance(left.getCenter(), right) <= left.getRadius();
+bool Overlap::isOverlapping(Shape::Circle circle, Shape::Point point){
+  return distance(circle.center, point.center) <= circle.radius;
 }
 
 //sqrt( (x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2 )
-double COLLISION::distance(const Vector3 &a, const Vector3 &b) 
+double Overlap::distance(Vector2D a, Vector2D b) 
 {
   return sqrt(pow(b.x() - a.x(), 2) + 
       pow(b.y() - a.y(), 2) + pow(b.z() - a.z(), 2));
-}
-
-//*TODO* research benefits of replacing vector with static arrays
-std::vector<double> COLLISION::calculateCollisionTimes(const MovingSphere& left, const MovingSphere& right)
-{
-  Vector3 pos = right.getCenter() - left.getCenter();
-  Vector3 vel = right.getVelocity() - left.getVelocity();
-  int radius = right.getRadius() + left.getRadius();
-
-  using std::vector;
-  vector<double> x_squared = POLYNOMIAL::square(vel.x(), pos.x());
-  vector<double> y_squared = POLYNOMIAL::square(vel.y(), pos.y());
-  vector<double> z_squared = POLYNOMIAL::square(vel.z(), pos.z());
-
-  vector<double> squared(3);
-   
-  for(int i = 0; i < 3; ++i) {
-    squared[i] = x_squared[i] + y_squared[i] + z_squared[i];
-  }  
-  squared[2] -= radius * radius; //subtract r^2 from both sides
-
-  return POLYNOMIAL::SecondDegreeSolutions<double>(squared);
 }
--- collision.h
+++ collision.h
@@ -1,37 +1,21 @@
 #ifndef LADIA_CSS342_COLLISION_H
 #define LADIA_CSS342_COLLISION_H
 
-#include "movingsphere.h"
-#include "polynomial.h"
+#include "Shape.h"
 
 // Namespace holding common functions useful for collsion detection
-namespace COLLISION {
-  typedef NVector<double, 3> Point;
+namespace Overlap 
+{
+  //return: true if the spheres are overlapping
+  bool isOverlapping(Shape::Circle, Shape::Circle);
 
-  //return: true if the two spheres are overlapping or touching
-  bool isOverlapping(const Sphere&, const Sphere&);
-
-  //return: true if the point is contained within the sphere
-  bool isOverlapping(const Sphere&, const Point&);
-
-  // return: true if the sphere intersects the plane
-  // bool isOverlapping(const Sphere&, const Plane&);
+  // return: true if the point is contained within the sphere
+  bool isOverlapping(Shape::Circle, Shape::Point);
 
   //return: the distance between two spheres using the distance formula
   //  d = sqrt( (xi2 - xi1)^2 )
-  double distance(const Vector3&, const Vector3&);
-
- 
-  // Returns a vector containing the times at which two moving spheres will
-  // collide. This function solves the quadratic equation created by using the
-  // distance formula to detect collisions. 
-  // The size of the returned vector will represent how many times the spheres
-  // collide with each other. 
-  // A size of 0 mean the spheres will never collide. A size of 1 means the 
-  // spheres will glance each other. A size of 2 means the glance each other 
-  // twice as they move through each other.
-  std::vector<double> calculateCollisionTimes(
-    const MovingSphere&, const MovingSphere&); 
+  double distance(Vector3D, Vector3D);
+  double distance(Vector2D, Vector2D);
 }
 
 #endif
