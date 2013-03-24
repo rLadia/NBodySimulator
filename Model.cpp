7ca6686e047a9d0c17a1559b1ec2e1fe1116523b
Rodelle Ladia Jr. <rLadia@gmail.com>
2013-03-23 19:08:00 -0700

    Stubs that reproduce NBodySimulator functionality

--- Model.cpp
+++ Model.cpp
@@ -1,14 +1,16 @@
 #include "Model.h"
+#include "nbodysimulator.h"
 
-void Model::Update(ObjectList &objects, double time)
+void Model::Update(ObjectList &objects, const double time)
 {
-  UpdateGravitationalForces(objects);
+  UpdateGravitationalForces(objects, time);
   HandleOverlaps(objects);
 }
 
-void Model::UpdateGravitationalForces(ObjectList &objects)
+void Model::UpdateGravitationalForces(ObjectList &objects, const double time)
 {
-
+  NBodySimulator simulator;
+  simulator.Simulate(objects, time);
 }
 
 void Model::HandleOverlaps(ObjectList &objects)
