fdcf5767921e06404d549ab1a478ffd162677991
Rodelle Ladia Jr. <rLadia@gmail.com>
2013-03-23 19:44:56 -0700

    Implemented simple shape interfaces

--- Shape.h
+++ Shape.h
@@ -1,12 +1,26 @@
 #ifndef LADIA_NBODYSIMULATOR_SHAPE_H
 #define LADIA_NBODYSIMULATOR_SHAPE_H
 
-class Shape
-{
-public:
-  Shape();
-private:
+#include "nvector.h"
 
-};
+namespace Shape
+{
+  struct Point
+  {
+    Vector2D center;
+  };
+
+  struct Circle
+  {
+    Vector2D center;
+    double radius;
+  };
+
+  struct Square
+  {
+    Vector2D center;
+    double length;
+  };
+}
 
 #endif
